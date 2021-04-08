d15dbabcc22e87b3bd3d8b90624e0ed0e54dab35
DARLANALONE <80855549+DARLANALONE@users.noreply.github.com>
2021-04-08 10:08:07 -0500

    PRIMER COMMIT
    ACTUALMENTE ESTOY REALIZANDO CAMBIOS

---
+++ storage/framework/views/ae0f52bb016d27a7753c130e7506eddcf3a82c77.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  </head>
+<body>
+<div class="container">
+    <div class="row col-md-12"></div>
+ <h1>Lorem ipsum dolor sit amet.</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur deserunt dolore dolorem eligendi exercitationem ipsum molestiae officiis quia repudiandae sapiente. Architecto culpa cum dolorum illo necessitatibus nobis odio repellat repellendus!
+    </p>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <br>
+    <i>francisco javier borda: KEY</i>
+</div>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\24marzo\resources\views/welcome.blade.php ENDPATH**/ ?>
